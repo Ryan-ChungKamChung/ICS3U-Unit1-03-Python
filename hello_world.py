71c28964497aa5d83c70463239a27bd3240cfd63
Ubuntu <ubuntu@ip-172-31-48-242.ec2.internal>
2020-11-17 23:46:09 +0000

    added hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Chung Kam Chung
+# Created in November 2020
+# "Hello, World!" with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
